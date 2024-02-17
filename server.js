bc4150738f7f9b0a15baec604835c927bfb48142
S.S.W.(ahiyantra) <45235557+ahiyantra@users.noreply.github.com>
2024-02-17 21:23:21 +0530

    updated "server[dot]js"

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcrypt'); /* step 1 */
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
@@ -10,43 +11,24 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+/*
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => { // step 2 
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => { // step 2 
+      console.log(res);
+    });
+});
+*/
 //END_ASYNC
 
 //START_SYNC
 
-
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds); // step 3 
+console.log(hash);
+let result = bcrypt.compareSync(myPlaintextPassword, hash); // step 3 
+console.log(result);
 
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 const PORT = process.env.PORT || 3000;
